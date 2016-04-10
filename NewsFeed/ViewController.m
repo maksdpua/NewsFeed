613857adfbfc5ed9fbbc3f52b246ca45496fb4c7
Maks <maksdpua@gmail.com>
2016-04-10 11:58:26 +0300

    Initial Commit

---
+++ NewsFeed/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewsFeed
+//
+//  Created by Maks on 4/10/16.
+//  Copyright © 2016 Maks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
