70388188a51912d4a07714d7d3e9de328d4afcba
Maks <maksdpua@gmail.com>
2016-04-11 17:25:56 +0300

    Fix presentation UIActivityViewController on iPad with share button

--- NewsFeed/GUI/ViewControllers/NFDetailController/NFDetailController.m
+++ NewsFeed/GUI/ViewControllers/NFDetailController/NFDetailController.m
@@ -9,13 +9,20 @@
 #import "NFDetailController.h"
 #import "MSCache.h"
 
+#define IDIOM    UI_USER_INTERFACE_IDIOM()
+#define IPAD     UIUserInterfaceIdiomPad
+
+
+
 @interface NFDetailController()
 
 @property (nonatomic, strong) IBOutlet UIWebView *webView;
 
 @end
 
-@implementation NFDetailController
+@implementation NFDetailController {
+    UIBarButtonItem *shareButton;
+}
 
 #pragma mark - Lifecycle
 
@@ -26,7 +33,7 @@
     [self.webView loadRequest:theRequest];
     
     
-    UIBarButtonItem *shareButton = [[UIBarButtonItem alloc] initWithTitle:@"Share" style:UIBarButtonItemStyleDone target:self action:@selector(shareAction)];
+    shareButton = [[UIBarButtonItem alloc] initWithTitle:@"Share" style:UIBarButtonItemStyleDone target:self action:@selector(shareAction)];
     
     self.navigationItem.rightBarButtonItem = shareButton;
 }
@@ -35,11 +42,20 @@
     NSString *textToShare = self.model.title;
     NSURL *myWebsite = [NSURL URLWithString:self.model.contentURL];
     UIImage *image = [UIImage imageWithData:[MSCache objectForKey:self.model.imageMedium]];
+    
     NSArray *objectsToShare = @[textToShare, image, myWebsite];
     
     UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:objectsToShare applicationActivities:nil];
     
+    if ( IDIOM == IPAD ) {
+        activityVC.modalPresentationStyle = UIModalPresentationPopover;
+        activityVC.popoverPresentationController.sourceView = self.view;
+        UIView *view = [shareButton valueForKey:@"view"];
+        activityVC.popoverPresentationController.sourceRect = view.frame;
+    }
     [self presentViewController:activityVC animated:YES completion:nil];
+ 
+    
 }
 
 @end
